52bff79c56f9094fb761f6dc78b05dd9bc8af015
Kelvin Gao <u6511753@au.edu>
2023-08-02 16:24:02 +0700

    Initial Commit

---
+++ SlotGame/SlotGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotGameApp.swift
+//  SlotGame
+//
+//  Created by Kelvin Gao  on 2/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlotGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
