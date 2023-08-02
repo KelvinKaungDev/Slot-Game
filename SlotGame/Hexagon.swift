dfe7bca67f327836d7d08c5b71438f52ccba3852
Kelvin Gao <u6511753@au.edu>
2023-08-02 21:36:35 +0700

    first commit

---
+++ SlotGame/Hexagon.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  SlotGame
+//
+//  Created by Kelvin Gao  on 2/8/2566 BE.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
